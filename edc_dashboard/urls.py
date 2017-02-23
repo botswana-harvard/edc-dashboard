f8f43cfb92390c96534b63f08c0cb09f9271d0f2
erikvw <ew2789@gmail.com>
2017-02-23 22:39:04 +0200

    add app_name

--- edc_dashboard/urls.py
+++ edc_dashboard/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+app_name = 'edc_dasboard'
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
