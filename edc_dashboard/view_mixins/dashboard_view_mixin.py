1f2d4e7122959a87ea3ac5a2fcc15b1a88bd20ed
erikvw <ew2789@gmail.com>
2017-09-07 01:22:41 +0200

    dont show requisition identifier if not drawn

--- edc_dashboard/view_mixins/dashboard_view_mixin.py
+++ edc_dashboard/view_mixins/dashboard_view_mixin.py
@@ -1,4 +1,5 @@
 from django.apps import apps as django_apps
+from edc_constants.constants import YES, NO
 
 
 class DashboardViewMixin:
@@ -6,3 +7,8 @@ class DashboardViewMixin:
     def get_template_names(self):
         return [django_apps.get_app_config(
             self.app_config_name).dashboard_template_name]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(YES=YES, NO=NO)
+        return context
