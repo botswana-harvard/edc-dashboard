32719e8c6370f1d8f6b996d17e7eb2b4709aabcf
erikvw <ew2789@gmail.com>
2017-10-09 12:40:12 +0200

    add test urls

--- edc_dashboard/urls.py
+++ edc_dashboard/urls.py
@@ -1,8 +1,20 @@
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url, include
 from django.contrib import admin
 
-app_name = 'edc_dasboard'
+app_name = 'edc_dashboard'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+
+if settings.APP_NAME == 'edc_dashboard':
+
+    from .tests import edc_dashboard_admin
+
+    urlpatterns = [
+        url(r'^admin/', edc_dashboard_admin.urls),
+        #         url(r'^listboard/', include('edc_dashboard_admin.tests.urls',
+        #                                     namespace='edc-model-wrapper')),
+    ]
