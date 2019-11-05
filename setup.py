dd09cd9862994605e6c4fcb8888f0a1acb1fcd8b
tumiemosweu <imosweuk@gmail.com>
2019-11-05 15:53:17 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='edc-dashboard',
-    version='0.2.9',
+    version='0.3.0',
     author=u'Software Engineering & Data Management',
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
